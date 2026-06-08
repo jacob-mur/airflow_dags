ddb9fd37093517c5f2b20e004d0c16aa4ada758c
Jacob Murray <96128065+jacob-murray@users.noreply.github.com>
2026-06-08 11:07:15 -0400

    updating

---
+++ dags/podman_runner.py
@@ -0,0 +1,34 @@
+from airflow.sdk import Asset, dag, task
+from pendulum import datetime, duration
+
+@dag(
+    start_date=datetime(2025, 4, 1),  # date after which the DAG can be scheduled
+    schedule="@daily",  # see: https://www.astronomer.io/docs/learn/scheduling-in-airflow for options
+    max_consecutive_failed_dag_runs=5,  # auto-pauses the DAG after 5 consecutive failed runs, experimental
+    doc_md=__doc__,  # add DAG Docs in the UI, see https://www.astronomer.io/docs/learn/custom-airflow-ui-docs-tutorial
+    default_args={
+        "owner": "jmurray",  # owner of this DAG in the Airflow UI
+        "retries": 3,  # tasks retry 3 times before they fail
+        "retry_delay": duration(seconds=5),  # tasks wait 30s in between retries
+    },  # default_args are applied to all tasks in a DAG
+    tags=["testing", "podman"],  # add tags in the UI
+    is_paused_upon_creation=True, # start running the DAG as soon as its created
+)
+
+def podman_runner():
+
+    # pull image
+    @task.bash
+    def pull_image(image_name: str = 'alpine:3.22.4') -> None:
+        #return "echo pulling image"
+        return f'podman pull "{image_name}"'    
+    
+    # run image
+    @task.bash
+    def run_image(image_name: str = 'alpine:3.22.4') -> None:
+        #return "echo running image"
+        return f'podman run --rm --privileged alpine:3.22.4 echo "hello world"'
+
+    # calling task
+    pull_image()
+    run_image()
