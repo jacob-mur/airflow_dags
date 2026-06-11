5101d3e04ba90008ad419b0a6b7fb66219c9ff65
Jacob Murray <96128065+jacob-murray@users.noreply.github.com>
2026-06-11 16:48:57 -0400

    updating podman_runner.py dag

--- podman_runner.py
+++ podman_runner.py
@@ -2,17 +2,11 @@ from airflow.sdk import Asset, dag, task
 from pendulum import datetime, duration
 
 @dag(
-    start_date=datetime(2025, 4, 1),  # date after which the DAG can be scheduled
+    start_date=datetime(2025, 4, 22),
     schedule="@daily",
-    max_consecutive_failed_dag_runs=5,  # auto-pauses the DAG after 5 consecutive failed runs, experimental
-    doc_md=__doc__, 
-    default_args={
-        "owner": "jmurray",  # owner of this DAG in the Airflow UI
-        "retries": 3,  # tasks retry 3 times before they fail
-        "retry_delay": duration(seconds=5),  # tasks wait 30s in between retries
-    },  # default_args are applied to all tasks in a DAG
-    tags=["testing", "podman"],  # add tags in the UI
-    is_paused_upon_creation=True, # start running the DAG as soon as its created
+    doc_md=__doc__,
+    default_args={"owner": "Jacob", "retries": 3},
+    tags=["example"],
 )
 
 def podman_runner():
